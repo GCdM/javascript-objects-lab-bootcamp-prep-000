8db233633d197c39e63061ea7af21f0cd98d7dcf
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-23 16:18:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var nRecipe = Object.assign(object, {key, value});
+  var nRecipe = Object.assign({}, object, {key, value});
   return nRecipe;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
