19eef39e5d45fbc92d63e577c048139abb3ee8c1
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-23 16:24:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 function deleteFromObjectByKey(object, key) {
-  delete object[key];
-  return object;
+  var nObject = Object.assign({}, object);
+  delete nObject[key];
+  return nObject;
 }
